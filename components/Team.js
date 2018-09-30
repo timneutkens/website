00815cc49878662a4bd2d832012f31bf456e9e26
Franco Arza <arzafran@gmail.com>
2018-09-30 17:48:07 -0300

    fix about thumbnails

--- components/Team.js
+++ components/Team.js
@@ -95,14 +95,14 @@ const Team = () => (
   <section>
     <TeamMember
       order={0}
-      thumbnail="jose.jpg"
+      thumbnail="Jose.jpg"
       name="Jose Rago"
       position="Co-Founder & Product Designer"
       description="As a passionate designer and pixel-perfect enthusiast, I'm always focused on solving user experience problems and improving design processes for clients and internal teams. I'm constantly with my finger on the pulse of the latest technology to create innovative products"
     />
     <TeamMember
       order={5}
-      thumbnail="fran.jpg"
+      thumbnail="Fran.jpg"
       name="Franco Arza"
       position="Co-Founder & Fullstack Developer"
       description="Former philosophy student, Full time programmer for five years (and counting) and web leader at basement.studio. Likes to gaze into the abyss, and into the horizon, because that's a cool thing to do on a profile picture. Always trying new stuff someone mentioned on reddit"
