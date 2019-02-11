892a8862f37a96dc86f4203aa336fcffccc83bd7
Franco Arza <arzafran@gmail.com>
2019-02-11 16:46:56 -0300

    use cache

--- next.config.js
+++ next.config.js
@@ -21,22 +21,22 @@ const nextConfig = {
       {
         urlPattern: /.svg$/,
         handler: 'cacheFirst'
-      },
-      {
-        urlPattern: /^https?.*/,
-        handler: 'networkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
       }
+      // {
+      //   urlPattern: /^https?.*/,
+      //   handler: 'networkFirst',
+      //   options: {
+      //     cacheName: 'https-calls',
+      //     networkTimeoutSeconds: 15,
+      //     expiration: {
+      //       maxEntries: 150,
+      //       maxAgeSeconds: 30 * 24 * 60 * 60
+      //     },
+      //     cacheableResponse: {
+      //       statuses: [0, 200]
+      //     }
+      //   }
+      // }
     ]
   },
   exportPathMap: async function(defaultPathMap, { dev, dir, outDir }) {
